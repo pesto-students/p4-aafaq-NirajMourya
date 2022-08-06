4b5e74578bf977fcd60851b76a1163cc1825eda2
NirajMourya <nirajmourya786@gmail.com>
2022-08-06 22:16:19 +0530

    Completed Excercise 3.4

--- Week-3/Excercise 3.4/main.js
+++ Week-3/Excercise 3.4/main.js
@@ -1,6 +1,6 @@
 function createStack() 
 {
-    let items = [];
+    let items = [];                   // 'items' is encapsulated and will  only be accessible to methods within and not outside of createStack scope
     return {
         push(item)                    //Adds an item on stack 
         {
@@ -14,8 +14,35 @@ function createStack()
 
 const stack = createStack();         //An Object Containing two functions is returned
 //Since the stack object doesn't have any property with name 'items'
-//below operations are performed on the stack is done on the items array of the object's parent
+//below operations are performed on the stack is done on the items array present in it's lexical scope
 stack.push(10);
 stack.push(5);
 stack.pop();
 console.log(stack.items);  //Hence, when we try to access a property which does not exist it give us 'undefined'
+
+//In the below code the reason it's accessible outside is the 'items' variable is returned when createStack is called
+//to have encapsulation the variable should only be accessible within the class or methods
+/* 
+function createStack() 
+{
+    return
+    {
+        items:[],
+        push(item) 
+        {
+            this.items.push(item);
+        },
+        pop() 
+        {
+            return cthis.items.pop();
+        }
+    };
+}
+
+const stack = createStack();
+stack.push(10);
+stack.push(5);
+stack.pop();// => 5
+stack.items;// => [10]
+stack.items= [10,100,1000];// Encapsulation broken!
+*/
