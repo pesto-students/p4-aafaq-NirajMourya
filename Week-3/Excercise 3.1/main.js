2e65b5c930f7a130cab2fa86705622eed8e87963
NirajMourya <nirajmourya786@gmail.com>
2022-08-06 18:13:04 +0530

    Partially Completed excercise 3.4

--- Week-3/Excercise 3.1/main.js
+++ Week-3/Excercise 3.1/main.js
@@ -4,6 +4,22 @@ function add(a,b)
     return a+b;
 }
 
+//Defining memoize function
+function memoize(fn)
+{
+    const cache = new Map();
+     return function(...args)
+     {
+        if(cache.has(Key))
+        {
+            return cache.get(Key)
+        }
+
+        cache.set(Key,fn(...args))
+        return cache.get(Key)        
+     }
+}
+
 //Memoize function
 const memoizeAdd = memoize(add);
 
--- Week-3/Excercise 3.4/main.js
+++ Week-3/Excercise 3.4/main.js
@@ -1,30 +1,19 @@
 function createStack() 
 {
+    let items = [];
     return{
-        items:[],
         push(item) 
         {
-            this.items.push(item);
+            items.push(item);
         },
         pop() {
-            returnthis.items.pop();
+            return items.pop();
         }
     };
 }
 
-const stack=createStack();
+const stack = createStack();
 stack.push(10);
 stack.push(5);
-stack.pop();// => 5
-stack.items;// => [10]
-stack.items= [10,100,1000];// Encapsulationbroken!
-
-function createStack() {
-    // Write your code here...
-}
-
-const stack=createStack();
-stack.push(10);
-stack.push(5);
-stack.pop();// => 5
-stack.items;// => undefined
+stack.pop();
+console.log(stack.items);
