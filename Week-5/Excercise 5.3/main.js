723ddbaa1b91cae18dc9e57d9db72c3a01f2b7f2
NirajMourya <nirajmourya786@gmail.com>
2022-08-20 01:17:07 +0530

    Completed Excercise 3

--- Week-5/Excercise 5.3/main.js
+++ Week-5/Excercise 5.3/main.js
@@ -1,3 +1,5 @@
 const hasDuplicate = arr => new Set(arr).size !== arr.length
-console.log(hasDuplicate([1,5,-1,1]))// false
+console.log(hasDuplicate([1,5,-1,1]))// true
+console.log(hasDuplicate([1,5,-1,11]))// false
+
 
