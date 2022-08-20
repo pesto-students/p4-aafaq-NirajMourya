b351b7f75d034e68f99466073e102a381880a268
NirajMourya <nirajmourya786@gmail.com>
2022-08-20 17:38:33 +0530

    Optimized the second Assignment

--- Week-5/Excercise 5.3/main.js
+++ Week-5/Excercise 5.3/main.js
@@ -1,4 +1,9 @@
 const hasDuplicate = arr => new Set(arr).size !== arr.length
+//Below code has better space complexity as it does not use set to copy data of array
+/*const hasDuplicate = (arr) => { return arr.some((element, index) =>{
+    return arr.indexOf(element) !== index
+});
+ } */
 console.log(hasDuplicate([1,5,-1,1]))// true
 console.log(hasDuplicate([1,5,-1,11]))// false
 
