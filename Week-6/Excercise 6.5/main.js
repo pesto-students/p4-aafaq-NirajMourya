cfaee38614cebf4aeb77b99e105263ffc4e00d2d
NirajMourya <nirajmourya786@gmail.com>
2022-08-28 20:51:05 +0530

    Completed 5th Assignment

---
+++ Week-6/Excercise 6.5/main.js
@@ -0,0 +1,19 @@
+var countKDifference = function(nums, k) {
+    for(let i=0;i<nums.length;i++)
+    {
+        for(let j=i+1;j<nums.length;j++)
+        {
+            if(k == Math.abs(nums[i]-nums[j]))
+            {
+               return 1;
+            }
+        }
+    }
+    return 0;
+};
+
+console.log(countKDifference([3,2,1,5,4],2)); //return 1
+console.log(countKDifference([3,2,1,5,4],5)); //return 0
+
+//Time complexity is O((n-1)!)
+//Space complexity is O(1)
