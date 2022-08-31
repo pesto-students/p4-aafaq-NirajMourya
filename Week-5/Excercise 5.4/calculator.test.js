a2933e05a7918c4e3c980f5cc2a335a4c2cc32d6
NirajMourya <nirajmourya786@gmail.com>
2022-08-31 20:22:31 +0530

    Added negative test case for Assg 5

--- Week-5/Excercise 5.4/calculator.test.js
+++ Week-5/Excercise 5.4/calculator.test.js
@@ -5,14 +5,29 @@ describe("Calculator Tests", () => {
       var result = mathOperations.sum(1,2)
       expect(result).toBe(3);
     });
+
+    test('Adding 1 + "2" should return 12', () => {
+      var result = mathOperations.sum(1,"2")
+      expect(result).toBe("12");
+    });
     
     test("Subtracting 2 from 10 should return 8", () => {
       var result = mathOperations.diff(10,2)
       expect(result).toBe(8);
     });
+
+    test("Subtracting / from 10 should return NaN", () => {
+      var result = mathOperations.diff(10,"/")
+      expect(result).toBeNaN();
+    });
     
     test("Multiplying 2 and 8 should return 16", () => {
       var result = mathOperations.product(2,8)
       expect(result).toBe(16);
+    });
+
+    test("Multiplying 2 and undefined should return NaN", () => {
+      var result = mathOperations.product(2,undefined)
+      expect(result).toBeNaN();
     });
    })
