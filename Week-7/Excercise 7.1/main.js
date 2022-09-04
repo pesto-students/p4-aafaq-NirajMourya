fc91cc8c6ba4a15b39d54af4a0cf0d35ca5cfaa5
NirajMourya <nirajmourya786@gmail.com>
2022-09-05 00:40:01 +0530

    Optimized the second assignment

--- Week-7/Excercise 7.1/main.js
+++ Week-7/Excercise 7.1/main.js
@@ -1,4 +1,37 @@
-var reverseList = function(head) {
+class SLLNode
+{
+    constructor(value)
+    {
+       this.value = value;
+       this.next = null;
+    }
+}
+
+function CreateSLL(arr)
+{
+    let head = new SLLNode(arr[0]);
+    let curr = head;
+    for(let i = 1;i<arr.length;++i)
+    {
+       curr.next = new SLLNode(arr[i]);
+       curr = curr.next;
+    }
+    return head;
+}
+
+function traverseList(head)
+{
+    let curr = head;
+    while(curr !== null)
+    {
+        console.log(curr.value)
+        curr = curr.next;
+    }
+}
+
+//time complexity is O(n)
+//space complexity is O(1)
+function reverseList(head) {
     let prev = null;
     let current = head;
     let next;
@@ -12,5 +45,7 @@ var reverseList = function(head) {
     return prev;
 };
 
-//time complexity is O(n)
-//space complexity is O(1)
+let head = CreateSLL([1,2,3,4,5]);
+traverseList(head);
+head = reverseList(head);
+traverseList(head);
--- Week-7/Excercise 7.2/main.js
+++ Week-7/Excercise 7.2/main.js
@@ -1,20 +1,58 @@
-var rotateLeft = function(head, k) {
-  
-    for(let i =  0;i<k;i++)
+class SLLNode
+{
+    constructor(value)
     {
-        console.log(i);
-        let temp = head;
-        while(temp.next != null)
-            temp = temp.next;
-        temp.next = head;
-        top = head;
-        head = head.next
-        top.next = null;
-        
+       this.value = value;
+       this.next = null;
     }
-   return head;
+}
+
+function CreateSLL(arr)
+{
+    let head = new SLLNode(arr[0]);
+    let curr = head;
+    for(let i = 1;i<arr.length;++i)
+    {
+       curr.next = new SLLNode(arr[i]);
+       curr = curr.next;
+    }
+    return head;
+}
+
+function traverseList(head)
+{
+    let curr = head;
+    while(curr !== null)
+    {
+        console.log(curr.value)
+        curr = curr.next;
+    }
+}
+
+//time complexity is O(n)
+//space complexity is O(1)
+var rotateLeft = function(head, k) {
+    let tail = head, n = 1;
+    while(tail.next)
+    {
+        n++;
+        tail = tail.next;
+    }
+
+    k %= n;
+    if(k === 0)
+       return head;
+    tail.next = head;
+    let newTail = head;
+    while(--k)
+        newTail = newTail.next;
+    let newhead = newTail.next;
+    newTail.next = null;
+    return newhead;
     
 };
 
-//time complexity is O(k*n)
-//space complexity is O(1)
+let head = CreateSLL([1,2,3,4,5]);
+traverseList(head);
+head = rotateLeft(head, 28);
+traverseList(head);
