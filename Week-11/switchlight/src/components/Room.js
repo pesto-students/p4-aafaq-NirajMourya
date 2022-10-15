4d87e080c14ecf54c277e3f3aef01d48f2dfccff
NirajMourya <nirajmourya786@gmail.com>
2022-10-15 21:42:17 +0530

    Completed switchlight assignment using redux

--- Week-11/switchlight/src/components/Room.js
+++ Week-11/switchlight/src/components/Room.js
@@ -1,15 +1,23 @@
-import { useState } from "react";
+//import { useState } from "react";
+import { connect } from 'react-redux'
 
-function Room()
+function Room(props)
 {
-    const [isOn,setOn] = useState(true);
+    /*const [isOn,setOn] = useState(true);
     const lightedness = isOn ? "lit":"dark";
     function flipSwitch ()
     {
         setOn(!isOn);
+    }*/
+    function flipSwitch ()
+    {
+        props.dispatch({
+            type:'flipSwitch'
+        })
     }
+    const lightedness = props.isOn ? "lit":"dark";
      return (
-        <div class={`room ${lightedness}`}>
+        <div className={`room ${lightedness}`}>
            The Room is {lightedness}
            <br/>
            <button onClick={flipSwitch}>Flip</button>
@@ -17,4 +25,7 @@ function Room()
      )
 }
 
-export default Room;
+const mapStateToProps =(state) => ({ isOn: state.isOn })
+const ConnectedRoom = connect(mapStateToProps)(Room);   
+
+export default ConnectedRoom;
