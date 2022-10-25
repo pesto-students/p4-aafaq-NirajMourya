dac55cd6df2ba9e5a2edb5d28ffbb08ee8e41c38
NirajMourya <nirajmourya786@gmail.com>
2022-10-25 13:39:11 +0530

    Completed switchlight assignment using hooks

--- Week-11/switchlight/src/components/Room.js
+++ Week-11/switchlight/src/components/Room.js
@@ -1,21 +1,34 @@
 //import { useState } from "react";
-import { connect } from 'react-redux'
+//import { connect } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
 
 function Room(props)
 {
+    //Using State
     /*const [isOn,setOn] = useState(true);
     const lightedness = isOn ? "lit":"dark";
     function flipSwitch ()
     {
         setOn(!isOn);
     }*/
-    function flipSwitch ()
+    //Using Connect
+    /*function flipSwitch ()
     {
         props.dispatch({
             type:'flipSwitch'
         })
     }
-    const lightedness = props.isOn ? "lit":"dark";
+    const lightedness = props.isOn ? "lit":"dark";*/
+    //Using Hooks
+    const isOn = useSelector((state) => state.isOn);
+    const dispatch = useDispatch();
+    function flipSwitch ()
+    {
+       dispatch({
+            type:'flipSwitch'
+        })
+    }
+    const lightedness = isOn ? "lit":"dark";
      return (
         <div className={`room ${lightedness}`}>
            The Room is {lightedness}
@@ -25,7 +38,8 @@ function Room(props)
      )
 }
 
-const mapStateToProps =(state) => ({ isOn: state.isOn })
+/*const mapStateToProps =(state) => ({ isOn: state.isOn })
 const ConnectedRoom = connect(mapStateToProps)(Room);   
 
-export default ConnectedRoom;
+export default ConnectedRoom;*/
+export default Room;
